7c9958da3e2df269ff4617a93c030e3c69a7e3a9
dddd <140569929+lucasr125@users.noreply.github.com>
2023-09-08 18:43:43 -0300

    Update smoothie factory tycoon.lua

--- smoothie factory tycoon.lua
+++ smoothie factory tycoon.lua
@@ -1,1 +1,127 @@
 
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+	local Window = OrionLib:MakeWindow({Name = "Statue Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Statue Hub"})
+	local Tab = Window:MakeTab({Name = "Farming",Icon = "rbxassetid://4483345998",PremiumOnly = false})
+	local init = Window:MakeTab({Name = "Initialization",Icon = "rbxassetid://4483345998",PremiumOnly = false})
+
+	local ao --Auto Obby
+	local plot --Plot
+	local aj --Auto Jar
+	local ab --Auto Blender
+	local ac --Auto Crate
+	local abuy --Auto Buy
+	local fc --Fast Arm 
+
+	init:AddButton({Name = "Initialize Plot",Callback = function()
+		for i, v in pairs(game:GetService("Workspace").Tycoons:GetDescendants()) do
+			if v.Name == "Owner" and v.Value == game:GetService("Players").LocalPlayer then
+				plot = v.Parent
+				OrionLib:MakeNotification({Name = "Successfully Initialized Plot!",Content = "All farming features are now available",Image = "rbxassetid://4483345998",Time = 5})
+
+				Tab:AddToggle({Name = "Auto Obby",Default = false,Callback = function(Value)
+					ao = Value
+				end})
+
+				Tab:AddToggle({Name = "Auto Buy",Default = false,Callback = function(Value)
+					abuy = Value
+				end})
+
+				Tab:AddToggle({Name = "Auto Jar",Default = false,Callback = function(Value)
+					aj = Value
+				end})
+
+				Tab:AddToggle({Name = "Auto Crate",Default = false,Callback = function(Value)
+					ac = Value
+				end})
+
+				Tab:AddToggle({Name = "Auto Blend",Default = false,Callback = function(Value)
+					ab = Value
+				end})
+
+				Tab:AddToggle({Name = "Fast Arm",Default = false,Callback = function(Value)
+					fc = Value
+				end})
+
+				local ws = Instance.new("ScreenGui")
+				ws.Parent = game:GetService("CoreGui")
+
+				local wsf = Instance.new("Frame")
+				wsf.Parent = ws
+				wsf.BackgroundColor3 = Color3.new(355,355,355)
+				wsf.Size = UDim2.new(100,100,100)
+				ws.Enabled = false
+
+				Tab:AddToggle({Name = "White Screen/ Anti Lag",Default = false,Callback = function(Value)
+					ws.Enabled = Value
+				end})
+
+				game:GetService("RunService").Heartbeat:Connect(function()
+					if fc == true then
+						game:GetService("ReplicatedStorage").Remotes.Event.Animations.moveArm:FireServer()
+					end
+				end)
+
+				game:GetService("RunService").Heartbeat:Connect(function()
+					if aj == true and task.wait(1) then
+						for i, v in pairs(plot:GetDescendants()) do
+							if v.Name == "OpenDoorPrompt" and v.Parent.Parent.Parent.Parent.Parent.Parent == plot.ProcessingMachines then
+								if game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character.HumanoidRootPart then
+									game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = v.Parent.Parent.CFrame + Vector3.new(-1,2,2)
+									fireproximityprompt(v)
+								end
+							end
+						end
+					end
+				end)
+
+				game:GetService("RunService").Heartbeat:Connect(function()
+					if  ac == true and plot.ProcessingMachines.CratePackager.CratePackager:FindFirstChild("Crates") and #plot.ProcessingMachines.CratePackager.CratePackager.Crates.Crate.Jars:GetChildren() == 8 then
+						for i, v in pairs(plot:GetDescendants()) do
+							if v.Name == "OpenDoorPrompt" and v.Parent.Parent.Parent.Parent.Parent.Parent == plot.ProcessingMachines then
+								if game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character.HumanoidRootPart then
+									game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = --[[v.Parent.Parent.CFrame]] plot.ProcessingMachines.CratePackager.CratePackager.Button.Button.CFrame + Vector3.new(-1,2,2)
+									fireproximityprompt(v)
+								end
+							end
+						end
+					end
+				end)
+
+				game:GetService("RunService").Heartbeat:Connect(function()
+					if ab == true then
+						for i, v in pairs(plot:GetDescendants()) do
+							if v.Name == "ActivateBlender" and task.wait(.5) then
+								if game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character.HumanoidRootPart then
+									game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = v.Parent.Parent.CFrame + Vector3.new(-1,2,2)
+									fireproximityprompt(v)
+								end
+							end
+						end
+					end
+				end)
+
+				game:GetService("RunService").Heartbeat:Connect(function()
+					if game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character.HumanoidRootPart then
+						if abuy == true and task.wait(4) then
+							for i, v in pairs(plot.PurchaseButtons:GetDescendants()) do
+								if v.Name == "Button" and v:FindFirstChild("TouchInterest") and v.Parent.Name ~= "Rainbow Upgrader" and v.Parent.Name ~= "Rainbow Gem Upgrader" and v.Parent.Name ~= "Toggle Door Gamepass" and v.Parent.Name ~= "Toggle Door Gamepass 2" and v.Parent.Name ~= "Gold Blender" and v.Parent.Name ~= "Gold Dropper 1" and v.Parent.Name ~= "Gold Dropper 2" and v.Parent.Name ~= "Gold Dropper 3" then
+									firetouchinterest(v, game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, 0)
+									firetouchinterest(v, game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, 1)
+								end
+							end
+						end
+					end
+				end)
+			end
+		end
+	end})
+
+	game:GetService("RunService").Heartbeat:Connect(function()
+		if task.wait(5) and ao == true and game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character.Head then
+			firetouchinterest(game:GetService("Players").LocalPlayer.Character.Head, game:GetService("Workspace").Obbies.HardObby.Finish.Button, 0)
+			firetouchinterest(game:GetService("Players").LocalPlayer.Character.Head, game:GetService("Workspace").Obbies.HardObby.Finish.Button, 1)
+			wait(1)
+			firetouchinterest(game:GetService("Players").LocalPlayer.Character.Head, game:GetService("Workspace").Obbies.EasyObby.Finish.Button, 0)
+			firetouchinterest(game:GetService("Players").LocalPlayer.Character.Head, game:GetService("Workspace").Obbies.EasyObby.Finish.Button, 1)
+		end
+	end)
