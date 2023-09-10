8ac3dac797f86d5211915bbd51c63f43b7e070a3
dddd <140569929+lucasr125@users.noreply.github.com>
2023-09-10 01:17:18 -0300

    Update ability wars ( scriptmaster ).lua

--- ability wars ( scriptmaster ).lua
+++ ability wars ( scriptmaster ).lua
@@ -40,7 +40,7 @@ ANTI_VOID.Position = Vector3.new(63, 15, -9)
 ANTI_VOID.Size = Vector3.new(2048,1,2048)
 ANTI_VOID.CanCollide = false
 
- -- BUTTONS
+-- BUTTONS
 
 MAIN_TAB:AddToggle({
 	Name = "Punch-Aura",
@@ -48,26 +48,26 @@ MAIN_TAB:AddToggle({
 	Callback = function(bool)
 		getgenv().saura = bool
 
-      while getgenv().saura do
-         task.wait(.4)
-      for i,v in pairs(game:GetService("Players"):GetChildren()) do
-         if v.Character["Right Arm"]:FindFirstChild("SelectionBox") == nil and v ~= game.Players.LocalPlayer then
-         local target = v.Character.HumanoidRootPart
-         local mag = (target.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
-         if getgenv().aurareach > mag then
-         local args = {
-         [1] = workspace:WaitForChild(target.Parent.Name),
-         [2] = Vector3.new(target.Position),
-         [3] = 1.8757749795913696,
-         [4] = game:GetService("Players"):WaitForChild(target.Parent.Name).Character:FindFirstChild("Head")
-         }
+		while getgenv().saura do
+			task.wait(.4)
+			for i,v in pairs(game:GetService("Players"):GetChildren()) do
+				if v.Character["Right Arm"]:FindFirstChild("SelectionBox") == nil and v ~= game.Players.LocalPlayer then
+					local target = v.Character.HumanoidRootPart
+					local mag = (target.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
+					if getgenv().aurareach > mag then
+						local args = {
+							[1] = workspace:WaitForChild(target.Parent.Name),
+							[2] = Vector3.new(target.Position),
+							[3] = 1.8757749795913696,
+							[4] = game:GetService("Players"):WaitForChild(target.Parent.Name).Character:FindFirstChild("Head")
+						}
 
-         game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("Punch"):FireServer(unpack(args))
-      end
-         end
-            end
-               end
-	               end    
+						game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("Punch"):FireServer(unpack(args))
+					end
+				end
+			end
+		end
+	end    
 })
 
 MAIN_TAB:AddSlider({
@@ -79,7 +79,7 @@ MAIN_TAB:AddSlider({
 	Increment = 1,
 	ValueName = "REACH",
 	Callback = function(val)
-      getgenv().aurareach = val
+		getgenv().aurareach = val
 	end    
 })
 
@@ -101,7 +101,7 @@ MAIN_TAB:AddSlider({
 	ValueName = "WS",
 	Callback = function(val)
 		game.Players.LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed = val
-   end
+	end
 })
 
 MAIN_TAB:AddSlider({
@@ -126,48 +126,48 @@ MAIN_TAB:AddSlider({
 	Increment = 1,
 	ValueName = "Hitbox",
 	Callback = function(val)
-         if val ~= 0 then
-            game.Players.LocalPlayer.Character:WaitForChild("Hitbox").Size = Vector3.new(val,val,val)
-            game.Players.LocalPlayer.Character:WaitForChild("Hitbox").Transparency = .6
-         else
-            game.Players.LocalPlayer.Character:WaitForChild("Hitbox").Size = Vector3.new(1,2,1)
-            game.Players.LocalPlayer.Character:WaitForChild("Hitbox").Transparency = 0
-         end
-      end 
+		if val ~= 0 then
+			game.Players.LocalPlayer.Character:WaitForChild("Hitbox").Size = Vector3.new(val,val,val)
+			game.Players.LocalPlayer.Character:WaitForChild("Hitbox").Transparency = .6
+		else
+			game.Players.LocalPlayer.Character:WaitForChild("Hitbox").Size = Vector3.new(1,2,1)
+			game.Players.LocalPlayer.Character:WaitForChild("Hitbox").Transparency = 0
+		end
+	end 
 })
 
 local function boss()
-   if game.Workspace:FindFirstChild("Dog") then
-      return "Dog";
-   elseif game.Workspace:FindFirstChild("Cat") then
-      return "Cat";
-   elseif game.Workspace:FindFirstChild("Tree Lord") then
-      return "Tree Lord";
-   elseif game.Workspace:FindFirstChild("Mushroom King") then
-      return "Mushroom King";
-   elseif game.Workspace:FindFirstChild("Scarecrow") then
-      return "Scarecrow";
-   else
-	  return "none";
-   end
+	if game.Workspace:FindFirstChild("Dog") then
+		return "Dog";
+	elseif game.Workspace:FindFirstChild("Cat") then
+		return "Cat";
+	elseif game.Workspace:FindFirstChild("Tree Lord") then
+		return "Tree Lord";
+	elseif game.Workspace:FindFirstChild("Mushroom King") then
+		return "Mushroom King";
+	elseif game.Workspace:FindFirstChild("Scarecrow") then
+		return "Scarecrow";
+	else
+		return "none";
+	end
 end
 
 MISC_TAB:AddToggle({
 	Name = "Auto-Kill Bosses",
 	Default = false,
 	Callback = function(bool)
-      getgenv().autokillboss = bool
+		getgenv().autokillboss = bool
 		while getgenv().autokillboss == true and boss() ~= "none" do
-         local punch = boss()
-         task.wait(.1)
-         local args = {
-         [1] = workspace:WaitForChild(punch),
-         [2] = Vector3.new(workspace:WaitForChild(punch):WaitForChild("Right Leg").Position),
-         [3] = 1.8757749795913696,
-         [4] = workspace:WaitForChild(punch):WaitForChild("Right Leg")
-      }
-      game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("Punch"):FireServer(unpack(args))
-      end
+			local punch = boss()
+			task.wait(.1)
+			local args = {
+				[1] = workspace:WaitForChild(punch),
+				[2] = Vector3.new(workspace:WaitForChild(punch):WaitForChild("Right Leg").Position),
+				[3] = 1.8757749795913696,
+				[4] = workspace:WaitForChild(punch):WaitForChild("Right Leg")
+			}
+			game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("Punch"):FireServer(unpack(args))
+		end
 	end    
 })
 
@@ -197,8 +197,8 @@ MISC_TAB:AddToggle({
 BADGES_TAB:AddButton({
 	Name = "Get Boing",
 	Callback = function()
-      game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("BigMushroomEvent"):FireServer()
-  	end    
+		game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("BigMushroomEvent"):FireServer()
+	end    
 })
 
 local cake = true
@@ -207,15 +207,15 @@ BADGES_TAB:AddButton({
 	Name = "Get Cake",
 	Callback = function()
 		if cake == true then
-		cake = false
-      	local cframe = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+			cake = false
+			local cframe = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
 
-      	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(160,271,29)
-      	task.wait(.1)
-      	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = cframe
-		cake = true
-  	end    
-end
+			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(160,271,29)
+			task.wait(.1)
+			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = cframe
+			cake = true
+		end    
+	end
 })
 
 
@@ -225,31 +225,31 @@ BADGES_TAB:AddButton({
 	Name = "Get Ascend",
 	Callback = function()
 		if ascend == true then
-		if game.Players.LocalPlayer.leaderstats.Ability.Value == "Lemon" then
-			if game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Y < 240 then
-				ascend = false
-				game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("AbilityEvent"):FireServer(1)
-				task.wait(.5)
-      			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(147,19,148)
-				ascend = true
+			if game.Players.LocalPlayer.leaderstats.Ability.Value == "Lemon" then
+				if game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Y < 240 then
+					ascend = false
+					game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("AbilityEvent"):FireServer(1)
+					task.wait(.5)
+					game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(147,19,148)
+					ascend = true
+				else
+					OrionLib:MakeNotification({
+						Name = "Enter Arena",
+						Content = "You need to enter the arena for this to work.",
+						Image = "rbxassetid://4483345998",
+						Time = 3
+					})
+				end
 			else
 				OrionLib:MakeNotification({
-					Name = "Enter Arena",
-					Content = "You need to enter the arena for this to work.",
+					Name = "Lemon Ability Required",
+					Content = "You need lemon for this to work.",
 					Image = "rbxassetid://4483345998",
 					Time = 3
 				})
 			end
-		else
-			OrionLib:MakeNotification({
-				Name = "Lemon Ability Required",
-				Content = "You need lemon for this to work.",
-				Image = "rbxassetid://4483345998",
-				Time = 3
-			})
-		end
-  	end  
-end  
+		end  
+	end  
 })
 
 
@@ -258,36 +258,36 @@ local shopping = true
 BADGES_TAB:AddButton({
 	Name = "Get Grocery Shopping",
 	Callback = function()
-	if shopping == true then
-		shopping = false
-      	local cframe = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+		if shopping == true then
+			shopping = false
+			local cframe = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
 
-      	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(160,271,29)
-      	task.wait(.1)
-	 	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-101, 23, -97)
-	 	task.wait(.1)
-	 	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(150, -21, 150)
-	 	task.wait(.1)
-    	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = cframe
-		shopping = true
-  	end  
-end 
+			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(160,271,29)
+			task.wait(.1)
+			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-101, 23, -97)
+			task.wait(.1)
+			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(150, -21, 150)
+			task.wait(.1)
+			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = cframe
+			shopping = true
+		end  
+	end 
 })
 
 BADGES_TAB:AddButton({
 	Name = "Get Backrooms",
 	Callback = function()
-	if game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Y < 240 then
-      	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-121, 19, -119)
-	else
-		OrionLib:MakeNotification({
-			Name = "Enter Arena",
-			Content = "You need to enter the arena for this to work.",
-			Image = "rbxassetid://4483345998",
-			Time = 5
-		})
-  	end    
-end
+		if game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Y < 240 then
+			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-121, 19, -119)
+		else
+			OrionLib:MakeNotification({
+				Name = "Enter Arena",
+				Content = "You need to enter the arena for this to work.",
+				Image = "rbxassetid://4483345998",
+				Time = 5
+			})
+		end    
+	end
 })
 
 SECRETS_TAB:AddButton({
@@ -366,27 +366,27 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
 			Default = false,
 			Callback = function(bool)
 				getgenv().aura = bool
-		
-			  while getgenv().aura do
-				 task.wait(.001)
-			  for i,v in pairs(game:GetService("Players"):GetChildren()) do
-				 if v.Character["Right Arm"]:FindFirstChild("SelectionBox") == nil and v ~= game.Players.LocalPlayer then
-				 local target = v.Character.HumanoidRootPart
-				 local mag = (target.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
-				 if getgenv().aurareach > mag then
-				 local args = {
-				 [1] = workspace:WaitForChild(target.Parent.Name),
-				 [2] = Vector3.new(target.Position),
-				 [3] = 1.8757749795913696,
-				 [4] = game:GetService("Players"):WaitForChild(target.Parent.Name).Character:FindFirstChild("Head")
-				 }
-		
-				 game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("Punch"):FireServer(unpack(args))
-			  end
-				 end
+
+				while getgenv().aura do
+					task.wait(.001)
+					for i,v in pairs(game:GetService("Players"):GetChildren()) do
+						if v.Character["Right Arm"]:FindFirstChild("SelectionBox") == nil and v ~= game.Players.LocalPlayer then
+							local target = v.Character.HumanoidRootPart
+							local mag = (target.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
+							if getgenv().aurareach > mag then
+								local args = {
+									[1] = workspace:WaitForChild(target.Parent.Name),
+									[2] = Vector3.new(target.Position),
+									[3] = 1.8757749795913696,
+									[4] = game:GetService("Players"):WaitForChild(target.Parent.Name).Character:FindFirstChild("Head")
+								}
+
+								game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("Punch"):FireServer(unpack(args))
+							end
+						end
 					end
-					   end
-						   end    
+				end
+			end    
 		})
 
 		DEV_MODE:AddColorpicker({
@@ -405,53 +405,53 @@ game.Players.LocalPlayer.Chatted:Connect(function(msg)
 			end    
 		})
 
-	DEV_MODE:AddButton({
-		Name = "Equip Jester (1500 PUNCHES)",
-		Callback = function()
-			game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("AbilitySelect"):FireServer("Jester")
-		end    
-	})
+		DEV_MODE:AddButton({
+			Name = "Equip Jester (1500 PUNCHES)",
+			Callback = function()
+				game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("AbilitySelect"):FireServer("Jester")
+			end    
+		})
 
-	DEV_MODE:AddButton({
-		Name = "Equip Spectator",
-		Callback = function()
-			game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("AbilitySelect"):FireServer("Spectator")
-		end    
-	})
+		DEV_MODE:AddButton({
+			Name = "Equip Spectator",
+			Callback = function()
+				game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("AbilitySelect"):FireServer("Spectator")
+			end    
+		})
 
-	DEV_MODE:AddButton({
-		Name = "Equip Cheese",
-		Callback = function()
-			game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("AbilitySelect"):FireServer("Cheese")
-		end    
-	})
+		DEV_MODE:AddButton({
+			Name = "Equip Cheese",
+			Callback = function()
+				game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("AbilitySelect"):FireServer("Cheese")
+			end    
+		})
 
-	DEV_MODE:AddButton({
-		Name = "Equip Burger (6000 PUNCHES)",
-		Callback = function()
-			game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("AbilitySelect"):FireServer("Burger")
-		end    
-	})
+		DEV_MODE:AddButton({
+			Name = "Equip Burger (6000 PUNCHES)",
+			Callback = function()
+				game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("AbilitySelect"):FireServer("Burger")
+			end    
+		})
 
-	DEV_MODE:AddButton({
-		Name = "Equip Seat",
-		Callback = function()
-			game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("AbilitySelect"):FireServer("Seat")
-		end    
-	})
-	
-	DEV_MODE:AddButton({
-		Name = "Equip Kick (MUST CLICK IN ARENA)",
-		Callback = function()
-			game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("BuyItem"):FireServer("Gain Kick")
-		end    
-	})
+		DEV_MODE:AddButton({
+			Name = "Equip Seat",
+			Callback = function()
+				game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("AbilitySelect"):FireServer("Seat")
+			end    
+		})
 
-	DEV_MODE:AddButton({
-		Name = "Equip Butter (5000 PUNCHES)",
-		Callback = function()
-			game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("AbilitySelect"):FireServer("Butter")
-		end    
-	})
-end
+		DEV_MODE:AddButton({
+			Name = "Equip Kick (MUST CLICK IN ARENA)",
+			Callback = function()
+				game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("BuyItem"):FireServer("Gain Kick")
+			end    
+		})
+
+		DEV_MODE:AddButton({
+			Name = "Equip Butter (5000 PUNCHES)",
+			Callback = function()
+				game:GetService("ReplicatedStorage"):WaitForChild("Remote Events"):WaitForChild("AbilitySelect"):FireServer("Butter")
+			end    
+		})
+	end
 end)
