dbccac673b1aba38fd75a8b28e5fdae5dbb4ac88
dddd <140569929+lucasr125@users.noreply.github.com>
2023-09-10 01:28:54 -0300

    Update HUB.lua

--- HUB.lua
+++ HUB.lua
@@ -1,23 +1,27 @@
-if game.PlaceId == 11727501024 then -- Create a Cart Ride
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/Create%20A%20Cart%20Ride%20Script.lua")))()
-elseif game.PlaceId == 183364845 then -- Speed Run 4
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/Speed%20Run%204")))()
-elseif game.PlaceId == 281489669 then -- Wizard Tycoon 2 PLRS
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/Wizard%20Tycoon")))()
-elseif game.PlaceId == 7419509075 then -- Cart Ride Car Used Car Battery
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/cartrideusedcarbatteyr.lua")))()
-elseif game.PlaceId == 4913581664 then -- Cart Ride Into Rdite
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/cartrintordite.lua")))()
-elseif game.PlaceId == 893973440 then -- Flee the Facility ( goofy script )
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/ftf.lua")))()
-elseif game.PlaceId == 13124109856 then -- Ice Cream Clicking ( another goofy script )
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/ice%20cream%20simulator%20script")))()
-elseif game.PlaceId == 142823291 then -- MM2 Esp ( i think it works on any mm2 copy too )
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/mm2%20esp.lua")))()
-elseif game.PlaceId == 4623386862 then -- Piggy get itens
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/piggy%20script.lua")))()
-elseif game.PlaceId == 10905034443 then -- Statue Hub ( i only use it for smoothie factory tycoon )
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/statue%20hub.lua")))()
-elseif game.PlaceId == 13780662126 then -- Yeet a plane ( i didnt even finish this )
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/yeet%20a%20plane.lua")))()
+if game.PlaceId == 11727501024 or game.PlaceId == 12081110790 then -- create a cart ride
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/Create%20A%20Cart%20Ride%20Script.lua")))()
+elseif game.PlaceId == 183364845 then -- sped run 4
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/Speed%20Run%204")))()
+elseif game.PlaceId == 281489669 then -- wizard tycoon
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/Wizard%20Tycoon")))()
+elseif game.PlaceId == 7419509075 then -- cart ride used car battery
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/cartrideusedcarbatteyr.lua")))()
+elseif game.PlaceId == 4913581664 then -- cart ride into rdite
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/cartrintordite.lua")))()
+elseif game.PlaceId == 893973440 then -- flee the facility
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/ftf.lua")))()
+elseif game.PlaceId == 13124109856 then -- ice cream clicking
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/ice%20cream%20simulator%20script")))()
+elseif game.PlaceId == 142823291 then -- mm2 role reveal
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/mm2%20esp.lua")))()
+elseif game.PlaceId == 4623386862 then -- piggy itens
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/piggy%20script.lua")))()
+elseif game.PlaceId == 10905034443 then -- random hub
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/statue%20hub.lua")))()
+elseif game.PlaceId == 13780662126 then -- yeet a plane
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/yeet%20a%20plane.lua")))()
+elseif game.PlaceId == 192800 then -- work at a pizza place OP
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/work%20at%20a%20pizza%20place%20script.lua")))()
+elseif game.PlaceId == 6305942109 then -- laundry simulator
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/laundry%20simulator.lua")))()
 end
