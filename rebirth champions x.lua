a2c9198a354af9afdba244059b94ff684a243522
dddd <140569929+lucasr125@users.noreply.github.com>
2023-09-14 15:57:40 -0300

    Create rebirth champions x.lua

---
+++ rebirth champions x.lua
@@ -0,0 +1,558 @@
+game:GetService("ReplicatedStorage").Events.Click3:FireServer()
+
+local ohString1 = "ClickMultiplier"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1)
+
+local ohString1 = "FreeAutoClicker"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1)
+
+local ohString1 = "RebirthButtons"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1)
+
+local ohString1 = "WalkSpeed"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1)
+
+local ohString1 = "GemsMultiplier"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1)
+
+local ohString1 = "PetEquip"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1)
+
+local ohString1 = "PetStorage"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1)
+
+local ohString1 = "LuckMultiplier"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1)
+
+local ohString1 = "FasterFreeAutoClicker"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1)
+
+local ohString1 = "ClickMultiplier"
+local ohString2 = "space"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+local ohString1 = "ChestCountdown"
+local ohString2 = "space"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+local ohString1 = "BankSpace"
+local ohString2 = "space"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+local ohString1 = "MaxCombo"
+local ohString2 = "space"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+local ohString1 = "PetEquip"
+local ohString2 = "space"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+local ohString1 = "LuckMultiplier"
+local ohString2 = "space"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+local ohString1 = "Teleport"
+local ohString2 = "space"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+local ohString1 = "GoldenChance"
+local ohString2 = "space"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+local ohString1 = "ToxicChance"
+local ohString2 = "space"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+local ohString1 = "ClickMultiplier"
+local ohString2 = "aqua"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+local ohString1 = "GemsMultiplier"
+local ohString2 = "aqua"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+local ohString1 = "PetEquip"
+local ohString2 = "aqua"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+local ohString1 = "HatchSpeed"
+local ohString2 = "aqua"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+local ohString1 = "LuckMultiplier"
+local ohString2 = "aqua"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+local ohString1 = "PetStorage"
+local ohString2 = "aqua"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+local ohString1 = "EvolutionSlot"
+local ohString2 = "aqua"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+local ohString1 = "EvolutionTime"
+local ohString2 = "aqua"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+local ohString1 = "DarkEvolutionChance"
+local ohString2 = "aqua"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+local ohString1 = "ClickMultiplier"
+local ohString2 = "fantasy"
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+
+local ohString1 = "GemsMultiplier"
+local ohString2 = "fantasy"
+
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+
+local ohString1 = "PetEquip"
+local ohString2 = "fantasy"
+
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+
+local ohString1 = "HatchSpeed"
+local ohString2 = "fantasy"
+
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+
+local ohString1 = "LuckMultiplier"
+local ohString2 = "fantasy"
+
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+
+local ohString1 = "PetStorage"
+local ohString2 = "fantasy"
+
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+
+local ohString1 = "ClickMultiplier"
+local ohString2 = "time"
+
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+
+local ohString1 = "GalaxyPetsChance"
+local ohString2 = "time"
+
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+
+local ohString1 = "PetEquip"
+local ohString2 = "time"
+
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+
+local ohString1 = "BankSlots"
+local ohString2 = "time"
+
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+
+local ohString1 = "LuckMultiplier"
+local ohString2 = "time"
+
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+
+local ohString1 = "PetStorage"
+local ohString2 = "time"
+
+game:GetService("ReplicatedStorage").Functions.Upgrade:InvokeServer(ohString1, ohString2)
+
+
+game:GetService("ReplicatedStorage").Functions.Aura:InvokeServer()
+
+
+game:GetService("ReplicatedStorage").Functions.TapSkin:InvokeServer()
+
+
+local ohString1 = "Forest"
+
+game:GetService("ReplicatedStorage").Events.CraftAmulet:FireServer(ohString1)
+
+
+local ohString1 = "Space"
+
+game:GetService("ReplicatedStorage").Events.CraftAmulet:FireServer(ohString1)
+
+
+local ohString1 = "Aqua"
+
+game:GetService("ReplicatedStorage").Events.CraftAmulet:FireServer(ohString1)
+
+
+local ohString1 = "Fantasy"
+
+game:GetService("ReplicatedStorage").Events.CraftAmulet:FireServer(ohString1)
+
+
+local ohString1 = "Free"
+local ohNumber2 = 1
+
+game:GetService("ReplicatedStorage").Events.Season:FireServer(ohString1, ohNumber2)
+
+
+local ohString1 = "Free"
+local ohNumber2 = 2
+
+game:GetService("ReplicatedStorage").Events.Season:FireServer(ohString1, ohNumber2)
+
+
+local ohString1 = "Free"
+local ohNumber2 = 3
+
+game:GetService("ReplicatedStorage").Events.Season:FireServer(ohString1, ohNumber2)
+
+
+local ohString1 = "Free"
+local ohNumber2 = 4
+
+game:GetService("ReplicatedStorage").Events.Season:FireServer(ohString1, ohNumber2)
+
+
+local ohString1 = "Free"
+local ohNumber2 = 5
+
+game:GetService("ReplicatedStorage").Events.Season:FireServer(ohString1, ohNumber2)
+
+
+local ohString1 = "Free"
+local ohNumber2 = 6
+
+game:GetService("ReplicatedStorage").Events.Season:FireServer(ohString1, ohNumber2)
+
+
+local ohString1 = "Free"
+local ohNumber2 = 7
+
+game:GetService("ReplicatedStorage").Events.Season:FireServer(ohString1, ohNumber2)
+
+
+local ohString1 = "Free"
+local ohNumber2 = 8
+
+game:GetService("ReplicatedStorage").Events.Season:FireServer(ohString1, ohNumber2)
+
+
+local ohString1 = "Free"
+local ohNumber2 = 9
+
+game:GetService("ReplicatedStorage").Events.Season:FireServer(ohString1, ohNumber2)
+
+
+local ohString1 = "Free"
+local ohNumber2 = 10
+
+game:GetService("ReplicatedStorage").Events.Season:FireServer(ohString1, ohNumber2)
+
+
+local ohString1 = "Free"
+local ohNumber2 = 11
+
+game:GetService("ReplicatedStorage").Events.Season:FireServer(ohString1, ohNumber2)
+
+
+local ohString1 = "Free"
+local ohNumber2 = 12
+
+game:GetService("ReplicatedStorage").Events.Season:FireServer(ohString1, ohNumber2)
+
+
+local ohString1 = "Free"
+local ohNumber2 = 13
+
+game:GetService("ReplicatedStorage").Events.Season:FireServer(ohString1, ohNumber2)
+
+
+local ohString1 = "Free"
+local ohNumber2 = 14
+
+game:GetService("ReplicatedStorage").Events.Season:FireServer(ohString1, ohNumber2)
+
+
+local ohString1 = "Nuclear"
+
+game:GetService("ReplicatedStorage").Events.Chest:FireServer(ohString1)
+
+
+local ohString1 = "Galaxy"
+
+game:GetService("ReplicatedStorage").Events.Chest:FireServer(ohString1)
+
+
+local ohString1 = "Viking"
+
+game:GetService("ReplicatedStorage").Events.Chest:FireServer(ohString1)
+
+
+local ohString1 = "Beach"
+
+game:GetService("ReplicatedStorage").Events.Chest:FireServer(ohString1)
+
+
+local ohString1 = "Fantasy"
+
+game:GetService("ReplicatedStorage").Events.Chest:FireServer(ohString1)
+
+
+local ohString1 = "Winter"
+
+game:GetService("ReplicatedStorage").Events.Chest:FireServer(ohString1)
+
+
+local ohString1 = "Haunted"
+
+game:GetService("ReplicatedStorage").Events.Chest:FireServer(ohString1)
+
+
+local ohString1 = "Hacker"
+
+game:GetService("ReplicatedStorage").Events.Chest:FireServer(ohString1)
+
+
+local ohString1 = "Hell"
+
+game:GetService("ReplicatedStorage").Events.Chest:FireServer(ohString1)
+
+
+local ohString1 = "Cyber"
+
+game:GetService("ReplicatedStorage").Events.Chest:FireServer(ohString1)
+
+
+local ohString1 = "Time"
+
+game:GetService("ReplicatedStorage").Events.Chest:FireServer(ohString1)
+
+
+local ohString1 = "Pirate"
+
+game:GetService("ReplicatedStorage").Events.Chest:FireServer(ohString1)
+
+
+local ohString1 = "Space"
+
+game:GetService("ReplicatedStorage").Events.Chest:FireServer(ohString1)
+
+
+local ohString1 = "Aqua"
+
+game:GetService("ReplicatedStorage").Events.Chest:FireServer(ohString1)
+
+
+local ohString1 = "Shadow"
+
+game:GetService("ReplicatedStorage").Events.Chest:FireServer(ohString1)
+
+
+local ohString1 = "Spawn"
+
+game:GetService("ReplicatedStorage").Events.Chest:FireServer(ohString1)
+
+
+game:GetService("ReplicatedStorage").Functions.Spin:InvokeServer()
+
+
+game:GetService("ReplicatedStorage").Functions.Machine:InvokeServer()
+
+
+local ohNumber1 = 1
+
+game:GetService("ReplicatedStorage").Events.DailyReward:FireServer(ohNumber1)
+
+
+local ohNumber1 = 2
+
+game:GetService("ReplicatedStorage").Events.DailyReward:FireServer(ohNumber1)
+
+
+local ohNumber1 = 3
+
+game:GetService("ReplicatedStorage").Events.DailyReward:FireServer(ohNumber1)
+
+
+local ohNumber1 = 4
+
+game:GetService("ReplicatedStorage").Events.DailyReward:FireServer(ohNumber1)
+
+
+local ohNumber1 = 5
+
+game:GetService("ReplicatedStorage").Events.DailyReward:FireServer(ohNumber1)
+
+
+local ohNumber1 = 6
+
+game:GetService("ReplicatedStorage").Events.DailyReward:FireServer(ohNumber1)
+
+
+local ohNumber1 = 7
+
+game:GetService("ReplicatedStorage").Events.DailyReward:FireServer(ohNumber1)
+
+
+local ohNumber1 = 8
+
+game:GetService("ReplicatedStorage").Events.DailyReward:FireServer(ohNumber1)
+
+
+local ohNumber1 = 9
+
+game:GetService("ReplicatedStorage").Events.DailyReward:FireServer(ohNumber1)
+
+
+local ohNumber1 = 10
+
+game:GetService("ReplicatedStorage").Events.DailyReward:FireServer(ohNumber1)
+
+
+local ohString1 = "175kthanks"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "jungle"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "anniversary"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "flower"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "bunny"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "fairy"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "omgfreex3luck"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "clover"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "dragon"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "time"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "bee"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "rainbow"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "zombie"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "swamp"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "frosty"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "sand"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "dino"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "rcx"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "sunflower"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "coconut"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "shiny"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "castle"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "cowboy"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "crystal"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "viking"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "military"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+local ohString1 = "futuristic"
+
+game:GetService("ReplicatedStorage").Events.Codes:FireServer(ohString1)
+
+
+
+local ohString1 = "CraftAll"
+local ohTable2 = {}
+
+game:GetService("ReplicatedStorage").Functions.Request:InvokeServer(ohString1, ohTable2)
