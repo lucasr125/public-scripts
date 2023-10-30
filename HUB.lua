f771bef5716586e53365bdfb12f5297d6a42892d
dddd <140569929+lucasr125@users.noreply.github.com>
2023-10-30 18:01:40 -0300

    Update HUB.lua

--- HUB.lua
+++ HUB.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 11727501024 or game.PlaceId == 12081110790 then -- create a cart ride
 	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/Create%20A%20Cart%20Ride%20Script.lua")))()
-elseif game.PlaceId == 183364845 then -- sped run 4
+elseif game.PlaceId == 183364845 then -- speed run 4
 	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/Speed%20Run%204")))()
 elseif game.PlaceId == 281489669 then -- wizard tycoon
 	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/Wizard%20Tycoon")))()
@@ -10,18 +10,29 @@ elseif game.PlaceId == 4913581664 then -- cart ride into rdite
 	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/cartrintordite.lua")))()
 elseif game.PlaceId == 893973440 then -- flee the facility
 	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/ftf.lua")))()
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/ftf2.lua")))()
 elseif game.PlaceId == 13124109856 then -- ice cream clicking
-	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/ice%20cream%20simulator%20script")))()
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/ice%20cream%20simulator%20script.lua")))()
 elseif game.PlaceId == 142823291 then -- mm2 role reveal
 	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/mm2%20esp.lua")))()
 elseif game.PlaceId == 4623386862 then -- piggy itens
 	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/piggy%20script.lua")))()
-elseif game.PlaceId == 10905034443 then -- random hub
+elseif game.PlaceId == 10905034443 then -- statue hub
 	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/statue%20hub.lua")))()
 elseif game.PlaceId == 13780662126 then -- yeet a plane
 	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/yeet%20a%20plane.lua")))()
-elseif game.PlaceId == 192800 then -- work at a pizza place OP
+elseif game.PlaceId == 192800 then -- work at a pizza place
 	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/work%20at%20a%20pizza%20place%20script.lua")))()
 elseif game.PlaceId == 6305942109 then -- laundry simulator
 	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/laundry%20simulator.lua")))()
+elseif game.PlaceId == 8260276694 then -- ability wars
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/ability%20wars%20(%20scriptmaster%20).lua")))()
+elseif game.PlaceId == 8137333410 then -- color by number
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/color%20by%20number.lua")))()
+elseif game.PlaceId == 662417684 then -- lucky block
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/luckyblock.lua")))()
+elseif game.PlaceId == 10905034443 then -- smoothie factory tycoon
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/smoothie%20factory%20tycoon.lua")))()
+elseif game.PlaceId == 10919241870 then -- rarity factory tycoon
+	loadstring(game:HttpGet(("https://raw.githubusercontent.com/lucasr125/public-scripts/main/rarity%20factory%20tycoon.lua")))()
 end
