ae85bd99ded597ea654734a6260a6d0b93395d41
dddd <140569929+lucasr125@users.noreply.github.com>
2023-10-30 18:29:52 -0300

    Update smoothie factory tycoon.lua

--- smoothie factory tycoon.lua
+++ smoothie factory tycoon.lua
@@ -76,7 +76,7 @@ Tab:AddToggle({Name = "Auto Jar",Default = false,Callback = function(Value)
 	aj = Value
 	while aj do
 		if aj == true then
-			for i, v in pairs(plot:GetDescendants()) do
+			for i, v in pairs(plot.ProcessingMachines.JarFactory:GetDescendants()) do
 				if v.Name == "OpenDoorPrompt" and v.Parent.Parent.Parent.Parent.Name == "JarFactory"  then
 					if tostring(v.Parent.Cooldown.TextLabel.Text) == "0" or tostring(v.Parent.Cooldown.TextLabel.Text) == "?" then
 						if game:GetService("Players").LocalPlayer.Character and game:GetService("Players").LocalPlayer.Character.HumanoidRootPart then
