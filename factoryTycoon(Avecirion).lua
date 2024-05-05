6d981308cd55613d768416d09a8ed1a3d5ffd988
dddd <140569929+lucasr125@users.noreply.github.com>
2024-05-05 15:03:43 -0300

    Update factoryTycoon(Avecirion).lua

--- factoryTycoon(Avecirion).lua
+++ factoryTycoon(Avecirion).lua
@@ -1,12 +1,24 @@
+local gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+
+local autoCollectMoney = false
+local autoBuyItems = false
+
 local plr = game.Players.LocalPlayer
 local tycoonOwned = plr:FindFirstChild("TycoonOwned").Value
 
 
-local enabledAutoCollect = false
-local autoBuyItems = false
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))();
+local Window = OrionLib:MakeWindow({Name = gameName.." ; "..identifyexecutor(), HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"});
+local Tab = Window:MakeTab({Name = "Main Tab",Icon = "rbxassetid://4483345998", PremiumOnly = false});
+Tab:AddToggle({Name = "Auto collect money",Default = false,Callback = function(Value)
+	autoCollectMoney = Value
+end});
+Tab:AddToggle({Name = "Auto buy items",Default = false,Callback = function(Value)
+	autoBuyItems = Value
+end});
 
 while wait(0.1) do
-	if enabledAutoCollect == true then
+	if autoCollectMoney == true then
 		local collector = tycoonOwned.Build.Collect.Part
 		firetouchinterest(plr.Character.HumanoidRootPart, collector, 1)
 		firetouchinterest(plr.Character.HumanoidRootPart, collector, 0)
@@ -14,7 +26,7 @@ while wait(0.1) do
 	end
 	if autoBuyItems == true then
 		for i, v in pairs(tycoonOwned.Buttons:GetDescendants()) do
-			if v.Name == "Part" and v.Parent.Name == "Button" and v:FindFirstChild("TouchInterest") and v.BrickColor == "Shamrock" then
+			if v.Name == "Part" and v.Parent.Name == "Button" and v:FindFirstChild("TouchInterest") and v.BrickColor == BrickColor.new("Shamrock") then
 				firetouchinterest(plr.Character.HumanoidRootPart, v, 1)
 				firetouchinterest(plr.Character.HumanoidRootPart, v, 0)
 				wait()
