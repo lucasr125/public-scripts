903e8b4334e0a7e93d827f63d8ccd63a2cec27f9
dddd <140569929+lucasr125@users.noreply.github.com>
2024-06-01 12:11:56 -0300

    Update pull a sword.lua

--- pull a sword.lua
+++ pull a sword.lua
@@ -1,114 +1,150 @@
-getgenv().settings = {
-	AutoClick = false,
-	AutoRebirth = false,
-	AutoClaimGift = false,
-	AutoEventPets = false
+local localPlayer = game.Players.LocalPlayer
+local gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+
+local settings = {
+	["walkspeed"] = 16,
+	["jumppower"] = 50,
+	["gravity"] = 196.6,
+	["hipheight"] = 2,
+	["autowalkspeed"] = false,
+	["autojumppower"] = false,
+	["autohipheight"] = false,
+	["autorebirth"] = false,
+	["autoclick"] = false,
 }
-local LocalPlr = game.Players.LocalPlayer
-local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
-local Bracket = loadstring(game:HttpGet("https://raw.githubusercontent.com/lucasr125/bracket-lib-v3.3/main/bracketv3.3.lua"))()
 
-local Window = Bracket:Window({Name = GameName,Enabled = true,Color = Color3.new(1,0.5,0.25),Size = UDim2.new(0,296,0,296),Position = UDim2.new(0.5,-248,0.5,-248)}) do
-local MainTab = Window:Tab({Name = "Main"}) do
-local GameFunc = MainTab:Section({Name = "Game Functions",Side = "Left"}) do
-	local AutoClick = GameFunc:Toggle({Name = "Auto Click",Flag = "AutoClick",Side = "Left",Value = false,Callback = function(Toggle_Bool) 
-	getgenv().settings.AutoClick = Toggle_Bool
-	end})
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/lucasr125/Bracket_Orion/main/orionlib.lua')))();
+local Window = OrionLib:MakeWindow({Name = gameName.." ; "..identifyexecutor(), HidePremium = false, SaveConfig = false, IntroEnabled = true, IntroText = gameName});
 
-	local AutoRebirth = GameFunc:Toggle({Name = "Auto Rebirth",Flag = "AutoRebirth",Side = "Left",Value = false,Callback = function(Toggle_Bool) 
-	getgenv().settings.AutoRebirth = Toggle_Bool
-	end})
+local localTab = Window:MakeTab({Name = "Local",Icon = "rbxassetid://4483345998",PremiumOnly = false});
+local SetValueSection = localTab:AddSection({
+	Name = "Set Values Section"
+})
+local walkspeedSlider = SetValueSection:AddSlider({Name = "Set walkspeed",Min = 0,Max = 1000,Default = settings.walkspeed,Color = Color3.fromRGB(255,255,255),Increment = 1,ValueName = "walkspeed",Callback = function(Value)
+	settings.walkspeed = Value
+	localPlayer.Character.Humanoid.WalkSpeed = settings.walkspeed
+end});
+local jumppowerSlider = SetValueSection:AddSlider({Name = "Set jumppower",Min = 0,Max = 1000,Default = settings.jumppower,Color = Color3.fromRGB(255,255,255),Increment = 1,ValueName = "jumppower",Callback = function(Value)
+	settings.jumppower = Value
+	localPlayer.Character.Humanoid.JumpPower = settings.jumppower
+	localPlayer.Character.Humanoid.UseJumpPower = true
+end});
+local hipheightSlider = SetValueSection:AddSlider({Name = "Set hipheight",Min = 0,Max = 50,Default = settings.hipheight,Color = Color3.fromRGB(255,255,255),Increment = 1,ValueName = "hipheight",Callback = function(Value)
+	settings.hipheight = Value
+	localPlayer.Character.Humanoid.HipHeight = settings.hipheight
+end});
+local gravitySlider = SetValueSection:AddSlider({Name = "Set gravity",Min = 0,Max = 1000,Default = settings.gravity,Color = Color3.fromRGB(255,255,255),Increment = 1,ValueName = "gravity",Callback = function(Value)
+	settings.gravity = Value
+	game.Workspace.Gravity = settings.gravity
+end});
+local AutoValueSection = localTab:AddSection({
+	Name = "Auto Values Section"
+})
+local walkspeedToggle = AutoValueSection:AddToggle({Name = "Auto walkspeed",Default = false,Callback = function(Value)
+	settings.autowalkspeed = Value
+	if settings.autowalkspeed == true then
+		while task.wait(0.1) and settings.autowalkspeed == true do
+			localPlayer.Character.Humanoid.WalkSpeed = settings.walkspeed
+		end
+	end
+end});
+local jumppowerToggle = AutoValueSection:AddToggle({Name = "Auto jumppower",Default = false,Callback = function(Value)
+	settings.autojumppower = Value
+	if settings.autojumppower == true then
+		while task.wait(0.1) and settings.autojumppower == true do
+			localPlayer.Character.Humanoid.JumpPower = settings.jumppower
+			localPlayer.Character.Humanoid.UseJumpPower = true
+		end
+	end
+end});
+local hipheightToggle = AutoValueSection:AddToggle({Name = "Auto hipheight",Default = false,Callback = function(Value)
+	settings.autohipheight = Value
+	if settings.autohipheight == true then
+		while task.wait(0.1) and settings.autohipheight == true do
+			localPlayer.Character.Humanoid.HipHeight = settings.hipheight
+		end
+	end
+end});
 
-	local AutoClaimGift = GameFunc:Toggle({Name = "Auto Claim Gift",Flag = "AutoClaimGift",Side = "Left",Value = false,Callback = function(Toggle_Bool) 
-	getgenv().settings.AutoClaimGift = Toggle_Bool
-	end})
-
-	local AutoEventPets = GameFunc:Toggle({Name = "Auto Get Event Pets",Flag = "AutoEventPets",Side = "Left",Value = false,Callback = function(Toggle_Bool) 
-	getgenv().settings.AutoEventPets = Toggle_Bool
-	end})
-
-	local GetBestClick = GameFunc:Button({Name = "Get Best Click",Side = "Left",Callback = function() 
+local mainTab = Window:MakeTab({Name = "Main",Icon = "rbxassetid://4483345998",PremiumOnly = false});
+local MainSection = mainTab:AddSection({
+	Name = "Main Section"
+})
+local autoclickToggle = MainSection:AddToggle({Name = "Auto click",Default = false,Callback = function(Value)
+	settings.autoclick = Value
+	if settings.autoclick == true then
+		while task.wait(0.1) and settings.autoclick == true do
+			local ohString1 = "Click"
+			local ohBoolean2 = false
+			game:GetService("ReplicatedStorage").ClickEvent:FireServer(ohString1, ohBoolean2)
+		end
+	end
+end});
+local autorebirthToggle = MainSection:AddToggle({Name = "Auto rebirth",Default = false,Callback = function(Value)
+	settings.autorebirth = Value
+	if settings.autorebirth == true then
+		while task.wait(0.1) and settings.autorebirth == true do
+			local ohString1 = "MAX"
+			game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.RebirthEvent:FireServer(ohString1)
+			game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.SuperRebirthEvent:FireServer()
+		end
+	end
+end});
+local getgiftsButton = MainSection:AddButton({Name = "Get gifts",Callback = function()
+	local rewardclaim = "Reward1"
+	game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)		
+	local rewardclaim = "Reward2"
+	game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
+	local rewardclaim = "Reward3"
+	game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
+	local rewardclaim = "Reward4"
+	game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
+	local rewardclaim = "Reward5"
+	game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
+	local rewardclaim = "Reward6"
+	game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
+	local rewardclaim = "Reward7"
+	game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
+	local rewardclaim = "Reward8"
+	game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
+	local rewardclaim = "Reward9"
+	game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
+	local rewardclaim = "Reward10"
+	game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
+	local rewardclaim = "Reward11"
+	game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
+	local rewardclaim = "Reward12"
+	game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
+end});
+local getbestclickerButton = MainSection:AddButton({Name = "Get best clicker",Callback = function()
 	for _, v in pairs(game.ReplicatedStorage.Items.Pencil:GetDescendants()) do
-	if v.Name == "PowerReq" then
-		if LocalPlr.Rock.Value >= v.Value then 
-			local PencilSelected = game:GetService("ReplicatedStorage").Items.Pencil[v.Parent.Parent.Name]
-			game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClickChangeEvent:FireServer(PencilSelected)
-		end 
+		if v.Name == "PowerReq" then
+			if localPlayer.Rock.Value >= v.Value then 
+				local PencilSelected = game:GetService("ReplicatedStorage").Items.Pencil[v.Parent.Parent.Name]
+				game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClickChangeEvent:FireServer(PencilSelected)
+				task.wait()
+			end 
+		end
 	end
-end
-	end})
-
-	local RedeemAllCodes = GameFunc:Button({Name = "Redeem All Codes",Side = "Left",Callback = function() 
+end});
+local redeemallcodesButton = MainSection:AddButton({Name = "Redeem all codes",Callback = function()
 	for _, v in pairs(game.ReplicatedStorage.Codes:GetDescendants()) do
-	if v.Name == "Code" then
-		game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.CodeEventSR:FireServer(v.Value)
+		if v.Name == "Code" then
+			game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.CodeEventSR:FireServer(v.Value)
+			task.wait()
+		end
 	end
-end
-	end})	
-end
-end
-end
-
-game:GetService("RunService").Heartbeat:Connect(function()
-	if getgenv().settings.AutoClick == true then
-local ohString1 = "Click"
-local ohBoolean2 = false
-
-game:GetService("ReplicatedStorage").ClickEvent:FireServer(ohString1, ohBoolean2)
+end});
+local RetroEventSection = mainTab:AddSection({
+	Name = "Retro Event Section"
+})
+local autoclickToggle = RetroEventSection:AddToggle({Name = "Auto click",Default = false,Callback = function(Value)
+	settings.autoclick = Value
+	if settings.autoclick == true then
+		while task.wait(0.1) and settings.autoclick == true do
+			local ohString1 = "Click"
+			local ohBoolean2 = true
+			game:GetService("ReplicatedStorage").ClickEvent:FireServer(ohString1, ohBoolean2)
+		end
 	end
-end)
-
-game:GetService("RunService").Heartbeat:Connect(function()
-	if getgenv().settings.AutoRebirth == true then
-		game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.RebirthEvent:FireServer()
-game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.SuperRebirthEvent:FireServer()
-	end
-end)
-
-game:GetService("RunService").Heartbeat:Connect(function()
-	if getgenv().settings.AutoClaimGift == true then
-		local rewardclaim = "Reward1" -- 1 a 12
-		game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
-		
-		local rewardclaim = "Reward2" -- 1 a 12
-		game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
-
-		local rewardclaim = "Reward3" -- 1 a 12
-		game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
-
-		local rewardclaim = "Reward4" -- 1 a 12
-		game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
-
-		local rewardclaim = "Reward5" -- 1 a 12
-		game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
-
-		local rewardclaim = "Reward6" -- 1 a 12
-		game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
-
-		local rewardclaim = "Reward7" -- 1 a 12
-		game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
-
-		local rewardclaim = "Reward8" -- 1 a 12
-		game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
-
-		local rewardclaim = "Reward9" -- 1 a 12
-		game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
-
-		local rewardclaim = "Reward10" -- 1 a 12
-		game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
-
-		local rewardclaim = "Reward11" -- 1 a 12
-		game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
-
-		local rewardclaim = "Reward12" -- 1 a 12
-		game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.ClaimGift:FireServer(rewardclaim)
-	end
-end)
-
-game:GetService("RunService").Heartbeat:Connect(function()
-	if getgenv().settings.AutoEventPets == true then
-		game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.SpaceEggEvent:FireServer()
-		game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.FreePackEvent:FireServer()
-		game:GetService("ReplicatedStorage").GameClient.Events.RemoteEvent.EventEggEvent:FireServer()
-	end
-end)
+end});
