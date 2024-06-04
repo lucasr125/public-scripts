8104826ad0c85a4d4d652cafe179159edd081f69
dddd <140569929+lucasr125@users.noreply.github.com>
2024-06-04 18:27:50 -0300

    Create SlapBattles_IceTrials.lua

---
+++ SlapBattles_IceTrials.lua
@@ -0,0 +1,23 @@
+local Window = OrionLib:MakeWindow({IntroText=GameName,IntroIcon="rbxassetid://15315284749",Name=(GameName .. " | " .. identifyexecutor()),IntroToggleIcon="rbxassetid://7734091286",HidePremium=false,SaveConfig=false,IntroEnabled=true,ConfigFolder="slap battles"});
+local Tab = Window:MakeTab({Name="Main",Icon="rbxassetid://4370318685",PremiumOnly=false});
+Tab:AddLabel("Owner Credits Script By [ Giang ]");
+Tab:AddLabel("DonjoSx Shared Script Me, GoodLuck");
+Tab:AddParagraph("Game's ID [ " .. game.PlaceId .. " ]", "Server ID [ " .. game.JobId .. " ]");
+local InfoServer = Tab:AddSection({Name="Misc"});
+Tab:AddButton({Name="Get Badge",Callback=function()
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-554, 177, 56);
+	wait(0.7);
+	for i, v in ipairs(game:GetService("Workspace"):GetDescendants()) do
+		if (v.ClassName == "ProximityPrompt") then
+			v.HoldDuration = 0;
+		end
+	end
+	wait(0.08);
+	for i, v in ipairs(game:GetService("Workspace"):GetDescendants()) do
+		if (v.ClassName == "ProximityPrompt") then
+			fireproximityprompt(v);
+		end
+	end
+	wait(1.5);
+	game:GetService("TeleportService"):Teleport(6403373529);
+end});
