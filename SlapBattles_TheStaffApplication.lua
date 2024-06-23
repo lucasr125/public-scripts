6e34b08c3a69906bae1d36ee71c00745a34ce512
dddd <140569929+lucasr125@users.noreply.github.com>
2024-06-23 19:37:26 -0300

    Update SlapBattles_TheStaffApplication.lua

--- SlapBattles_TheStaffApplication.lua
+++ SlapBattles_TheStaffApplication.lua
@@ -1,56 +1,36 @@
-local Window = OrionLib:MakeWindow({IntroText = (GameName), IntroIcon = "rbxassetid://15315284749",Name = (GameName.." | ".. identifyexecutor()),IntroToggleIcon = "rbxassetid://7734091286", HidePremium = false, SaveConfig = false, IntroEnabled = true, ConfigFolder = "slap battles"})
-
-local Tab = Window:MakeTab({
-	Name = "Misc",
-	Icon = "rbxassetid://4370318685",
-	PremiumOnly = false
-})
-
-Tab:AddLabel("Owner Credits Script By [ Giang ]")
-Tab:AddLabel("DonjoSx Shared Script Me, GoodLuck")
-local InfoServer = Tab:AddSection({Name = "Info"})
-CanYouFps = Tab:AddLabel("Your Fps [ "..math.floor(workspace:GetRealPhysicsFPS()).." ]")
-CanYouPing = Tab:AddLabel("Your Ping [ "..game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValueString().." ]")
-TimeServer = Tab:AddLabel("Server Time [ "..math.floor(workspace.DistributedGameTime / 60 / 60).." Hour | "..math.floor(workspace.DistributedGameTime / 60) - (math.floor(workspace.DistributedGameTime / 60 / 60) * 60).." Minute | "..math.floor(workspace.DistributedGameTime) - (math.floor(workspace.DistributedGameTime / 60) * 60).." Second ]")
-Tab:AddParagraph("Game's ID [ "..game.PlaceId.." ]","Server ID [ "..game.JobId.." ]")
-
-AutoSetInfo = Tab:AddToggle({
-	Name = "Auto Set Info",
-	Default = false,
-	Callback = function(Value)
-_G.AutoSetInfo = Value
-while _G.AutoSetInfo do
-CanYouFps:Set("Your Fps [ "..math.floor(workspace:GetRealPhysicsFPS()).." ]")
-CanYouPing:Set("Your Ping [ "..game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValueString().." ]")
-TimeServer:Set("Server Time [ "..math.floor(workspace.DistributedGameTime / 60 / 60).." Hour | "..math.floor(workspace.DistributedGameTime / 60) - (math.floor(workspace.DistributedGameTime / 60 / 60) * 60).." Minutes | "..math.floor(workspace.DistributedGameTime) - (math.floor(workspace.DistributedGameTime / 60) * 60).." Second ]")
-task.wait()
-end
-	end    
-})
-
-local InfoServer = Tab:AddSection({Name = "Misc"})
-
-Tab:AddButton({
-	Name = "Teleport Enter Final Room",
-	Callback = function()
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(502, 76, 59)
-task.wait(4)
-if getconnections then
-for i,v in next, getconnections(game.Players.LocalPlayer.Idled) do
-v:Disable() 
-end
-end
-OrionLib:MakeNotification({Name = "Error",Content = "Turned on Anti Afk",Image = "rbxassetid://7733658504",Time = 3})
-OrionLib:MakeNotification({Name = "Error",Content = "You have to wait the 1 hour, or you have view Info.",Image = "rbxassetid://7733658504",Time = 5})
-  	end    
-})
-
-Tab:AddButton({
-	Name = "[ Destroy GUI ] [ Toggle Gui ]",
-	Callback = function()
-OrionLib:Destroy()
-if game.Players.LocalPlayer.PlayerGui:FindFirstChild("ToggleUi") ~= nil then
-game.Players.LocalPlayer.PlayerGui:FindFirstChild("ToggleUi"):Destroy()
-end
-  	end 
-})
+local Window = OrionLib:MakeWindow({IntroText=GameName,IntroIcon="rbxassetid://15315284749",Name=(GameName .. " | " .. identifyexecutor()),IntroToggleIcon="rbxassetid://7734091286",HidePremium=false,SaveConfig=false,IntroEnabled=true,ConfigFolder="slap battles"});
+local Tab = Window:MakeTab({Name="Misc",Icon="rbxassetid://4370318685",PremiumOnly=false});
+Tab:AddLabel("Owner Credits Script By [ Giang ]");
+Tab:AddLabel("DonjoSx Shared Script Me, GoodLuck");
+local InfoServer = Tab:AddSection({Name="Info"});
+CanYouFps = Tab:AddLabel("Your Fps [ " .. math.floor(workspace:GetRealPhysicsFPS()) .. " ]");
+CanYouPing = Tab:AddLabel("Your Ping [ " .. game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValueString() .. " ]");
+TimeServer = Tab:AddLabel("Server Time [ " .. math.floor((workspace.DistributedGameTime / 60) / 60) .. " Hour | " .. (math.floor(workspace.DistributedGameTime / 60) - (math.floor((workspace.DistributedGameTime / 60) / 60) * 60)) .. " Minute | " .. (math.floor(workspace.DistributedGameTime) - (math.floor(workspace.DistributedGameTime / 60) * 60)) .. " Second ]");
+Tab:AddParagraph("Game's ID [ " .. game.PlaceId .. " ]", "Server ID [ " .. game.JobId .. " ]");
+AutoSetInfo = Tab:AddToggle({Name="Auto Set Info",Default=false,Callback=function(Value)
+	_G.AutoSetInfo = Value;
+	while _G.AutoSetInfo do
+		CanYouFps:Set("Your Fps [ " .. math.floor(workspace:GetRealPhysicsFPS()) .. " ]");
+		CanYouPing:Set("Your Ping [ " .. game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValueString() .. " ]");
+		TimeServer:Set("Server Time [ " .. math.floor((workspace.DistributedGameTime / 60) / 60) .. " Hour | " .. (math.floor(workspace.DistributedGameTime / 60) - (math.floor((workspace.DistributedGameTime / 60) / 60) * 60)) .. " Minutes | " .. (math.floor(workspace.DistributedGameTime) - (math.floor(workspace.DistributedGameTime / 60) * 60)) .. " Second ]");
+		task.wait();
+	end
+end});
+local InfoServer = Tab:AddSection({Name="Misc"});
+Tab:AddButton({Name="Teleport Enter Final Room",Callback=function()
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(502, 76, 59);
+	task.wait(4);
+	if getconnections then
+		for i, v in next, getconnections(game.Players.LocalPlayer.Idled) do
+			v:Disable();
+		end
+	end
+	OrionLib:MakeNotification({Name="Error",Content="Turned on Anti Afk",Image="rbxassetid://7733658504",Time=3});
+	OrionLib:MakeNotification({Name="Error",Content="You have to wait the 1 hour, or you have view Info.",Image="rbxassetid://7733658504",Time=5});
+end});
+Tab:AddButton({Name="[ Destroy GUI ] [ Toggle Gui ]",Callback=function()
+	OrionLib:Destroy();
+	if (game.Players.LocalPlayer.PlayerGui:FindFirstChild("ToggleUi") ~= nil) then
+		game.Players.LocalPlayer.PlayerGui:FindFirstChild("ToggleUi"):Destroy();
+	end
+end});
